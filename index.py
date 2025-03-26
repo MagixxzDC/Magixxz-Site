d4d6979b09e99f124d9c9c52b1fa8ec95d0ca893
Magixxz <dgarrett234@gmail.com>
2025-03-26 09:23:45 -0400

    Create index.py

---
+++ index.py
@@ -0,0 +1,220 @@
+Maincontent = """
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="icon" type="image/png" href="https://add.pics/images/2022/03/09/embotic.png">
+        <title>Magixxz | Main Menu</title>
+        <meta name="description" content="The main menu for Magixxz Website! Play games, unblock youtube videos, or look at cursed pictures..!">
+        <meta name="keywords" content="HTML, CSS, JavaScript">
+        <meta name="author" content="Magixxz">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <style>
+    . {
+  float: middle;
+  width: 50%;
+}
+    </style>
+
+    <style>
+    img:hover {
+    animation: shake 0.01s;
+    animation-iteration-count: infinite;
+}
+button:hover {
+  animation: shake 1s;
+  animation-iteration-count: infinite;
+}
+
+@keyframes shake {
+  0% { transform: translate(1px, 1px) rotate(0deg); }
+  10% { transform: translate(-1px, -2px) rotate(-1deg); }
+  20% { transform: translate(-3px, 0px) rotate(1deg); }
+  30% { transform: translate(3px, 2px) rotate(0deg); }
+  40% { transform: translate(1px, -1px) rotate(1deg); }
+  50% { transform: translate(-1px, 2px) rotate(-1deg); }
+  60% { transform: translate(-3px, 1px) rotate(0deg); }
+  70% { transform: translate(3px, 1px) rotate(-1deg); }
+  80% { transform: translate(-1px, -1px) rotate(1deg); }
+  90% { transform: translate(1px, 2px) rotate(0deg); }
+  100% { transform: translate(1px, -2px) rotate(-1deg); }
+}
+</style>
+<style>
+body {
+  background-image: url('https://add.pics/images/2022/03/09/MagixxzBanner.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
+</style>
+
+    <body bgcolor="#141414" class="w3-center">
+        <h1 class="w3-text-white" style="font-family:georgia;font-size:48px;font-style:italic;">Main Menu</h1>
+
+        <button style="background-color:#303030;color:#FFFFFF" class="w3-button w3-round w3-padding-large w3-large w3-hover-opacity"><a href="https://magixxz.pythonanywhere.com/chat">Go to Chat</a></button>
+                <button style="background-color:#303030;color:#FFFFFF" class="w3-button w3-round w3-padding-large w3-large w3-hover-opacity"><a href="https://magixxzunblock.pythonanywhere.com">Unblocker</a></button>
+                                                <button style="background-color:#303030;color:#FFFFFF" class="w3-button w3-round w3-padding-large w3-large w3-hover-opacity"><a href="https://magixxzgames.pythonanywhere.com">Games</a></button>
+                <button style="background-color:#303030;color:#FFFFFF" class="w3-button w3-round w3-padding-large w3-large w3-hover-opacity"><a href="https://magixxzcursedphotos.pythonanywhere.com/">Cursed Photos</a></button>
+                                <button style="background-color:#303030;color:#FFFFFF" class="w3-button w3-round w3-padding-large w3-large w3-hover-opacity"><a href="https://magixxzsuggestionbox.pythonanywhere.com">Suggestion Box</a></button>
+
+<br>
+<br>
+<script>
+var countDownDate = new Date("May 25, 2025 14:35:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "2024 School Year has ended.";
+  }
+}, 1000);
+</script>
+<script>
+var countDownDate2 = new Date("December 9, 2024 14:30:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate2 - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo2").innerHTML = hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo2").innerHTML = "School Day has ended.";
+  }
+}, 1000);
+</script>
+<p style="font-family:georgia; margin : 0; padding-top:0; font-size:30px" class="w3-text-orange"></p>
+<p style="margin:0; font-size:20px" class=" w3-text-white" id="demo"></p>
+<p style="font-family:georgia; margin : 0; padding-top:0; font-size:16px" class="w3-text-white">End of School Year</p>
+<p style="margin:0; font-size:20px" class=" w3-text-white" id="demo2"></p>
+<p style="font-family:georgia; margin : 0; padding-top:0; font-size:16px" class="w3-text-white">End of School Day</p>
+
+
+
+<style>
+div#main{
+    text-align: center;
+}
+html, body {
+.gallery{
+    display: -webkit-flex;
+    height: 100%;
+    -webkit-align-items: flex-end;
+    -webkit-justify-content: center;
+ }
+}
+
+.gallery img{
+    display: inline-block;
+    max-height: 10%;
+
+}
+.gallery img:target{
+    position: absolute;
+    top: 0;
+    max-height: 90%;
+    left: 50%;
+    -webkit-transform: translateX(-50%);
+}
+</style>
+        <div id="main">
+	    <div class="gallery">
+		<!--<a href="https://cheese.com"><img src="https://o.remove.bg/downloads/ea6eb0a3-87b0-4b88-8a9c-c255db48194e/Untitled_design__2___1_-removebg-preview.png" width="500" height="425" border="0">-->
+	    </div>
+        </div>
+</body>
+
+
+</html>"""
+Page2 = """<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Magixxz | Chat</title>
+        <meta name="description" content="This is where you can chat.. :)">
+        <meta name="keywords" content="HTML, CSS, JavaScript">
+        <meta name="author" content="Magixxz">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/png" href="https://add.pics/images/2022/03/09/embotic.png">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <!-- Load external CSS styles -->
+        <link rel="stylesheet" href="styles.css">
+    </head>
+
+
+    <body bgcolor="#141414" class="w3-center">
+<style>
+button:hover {
+  animation: shake 1s;
+  animation-iteration-count: infinite;
+}
+
+@keyframes shake {
+  0% { transform: translate(1px, 1px) rotate(0deg); }
+  10% { transform: translate(-1px, -2px) rotate(-1deg); }
+  20% { transform: translate(-3px, 0px) rotate(1deg); }
+  30% { transform: translate(3px, 2px) rotate(0deg); }
+  40% { transform: translate(1px, -1px) rotate(1deg); }
+  50% { transform: translate(-1px, 2px) rotate(-1deg); }
+  60% { transform: translate(-3px, 1px) rotate(0deg); }
+  70% { transform: translate(3px, 1px) rotate(-1deg); }
+  80% { transform: translate(-1px, -1px) rotate(1deg); }
+  90% { transform: translate(1px, 2px) rotate(0deg); }
+  100% { transform: translate(1px, -2px) rotate(-1deg); }
+}
+</style>
+
+        <h1 class="w3-text-white" style="font-family:georgia,garamond,serif;font-size:36px;font-style:italic;">Magixxz Chat</h1>
+
+                <button style="background-color:#303030;color:#FFFFFF" class="w3-button w3-round w3-padding-large w3-large w3-hover-opacity"><a href="https://magixxz.pythonanywhere.com">Main Menu</a></button>
+
+<br>
+<h1 class="w3-text-white" style="font-family:georgia,garamond,serif;font-size:28px;">They blocked the chat system..</h1>
+<h1 class="w3-text-white" style="font-family:georgia,garamond,serif;font-size:20px;">Rip Chat 2021-2022</h1>
+<h1 class="w3-text-white" style="font-family:georgia,garamond,serif;font-size:16px;">↓ Use this ↓</h1>
+
+
+                <button style="background-color:#303030;color:#FFFFFF" class="w3-button w3-round w3-padding-large w3-large w3-hover-opacity"><a href="https://pad.colibris-outilslibres.org/p/Magixxz">Click meee</a></button>
+
+<br>
+</div>
+
+</body>
+
+
+</html>"""
+def application(environ, start_response):
+    if environ.get('PATH_INFO') == '/':
+        status = '200 OK'
+        content = Maincontent
+    else:
+        status = 'Error'
+        content = Page2
+    response_headers = [('Content-Type', 'text/html'), ('Content-Length', str(len(content)))]
+    start_response(status, response_headers)
+    yield content.encode('utf8')
